06429aecb8b3a254724abda8e80b7990740ca00b
Zeki Gurbuz <zekidgurbuz@gmail.com>
2020-05-10 01:47:04 -0500

    Update prob01.java

--- Zeki/prob01.java
+++ Zeki/prob01.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class prob01 {
 	public static void main(String[] args) throws Exception {
-		Scanner file = new Scanner(new File("prob01-1-in.txt"));
+		Scanner file = new Scanner(new File("input.txt"));
 		String name = file.nextLine();
 		System.out.printf("Welcome to CodeWars, %s!%n", name);
 		file.close();
