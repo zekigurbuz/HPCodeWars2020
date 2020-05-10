3b989e9b591005ac010881ff231442a13fa2d01f
Zeki Gurbuz <zekidgurbuz@gmail.com>
2020-05-10 01:48:24 -0500

    Update prob04.java

--- Zeki/prob04.java
+++ Zeki/prob04.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class prob04 {
 	public static void main(String[] args) throws Exception {
-		Scanner file = new Scanner(new File("prob04-1-in.txt"));
+		Scanner file = new Scanner(new File("input.txt"));
 		BigInteger X = new BigInteger(file.nextLine()), Y = new BigInteger(file.nextLine());
 		BigInteger GCD = X.gcd(Y);
 		BigInteger LCM = X.multiply(Y).divide(GCD);
