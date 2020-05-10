53ca842e09007042a1be51d7ae80007e93780e13
Zeki Gurbuz <zekidgurbuz@gmail.com>
2020-05-10 01:49:18 -0500

    Update prob06.java

--- Zeki/prob06.java
+++ Zeki/prob06.java
@@ -10,7 +10,7 @@ public class prob06 {
 		return value_map.get(binary_search) + roman(arabic - binary_search);
 	}
 	public static void main(String[] args) throws Exception {
-		Scanner file = new Scanner(new File("prob06-2-in.txt"));
+		Scanner file = new Scanner(new File("input.txt"));
 		value_map = new TreeMap<>(); value_map.put(1, "B"); value_map.put(4, "BW"); value_map.put(5, "W"); value_map.put(9, "BK");
 		value_map.put(10, "Z"); value_map.put(40, "ZP"); value_map.put(50, "P"); value_map.put(90, "ZB");
 		value_map.put(100, "B"); value_map.put(400, "BB"); value_map.put(500, "G"); value_map.put(900, "BR");
