1a7a78b4e92be51f5f289d57863215be8efd21d2
Zeki Gurbuz <zekidgurbuz@gmail.com>
2020-05-10 01:48:35 -0500

    Update prob05.java

--- Zeki/prob05.java
+++ Zeki/prob05.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class prob05 {
 	public static void main(String[] args) throws Exception {
-		Scanner file = new Scanner(new File("prob05-3-in.txt"));
+		Scanner file = new Scanner(new File("input.txt"));
 		BigInteger X = new BigInteger(file.nextLine());
 		boolean isPrime = X.isProbablePrime(100);
 		System.out.printf("%s is %s%n", X.toString(), isPrime ? "PRIME" : "NOT Prime");
