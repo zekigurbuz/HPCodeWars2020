c1144f2b44873489f9d5c7ecde0755da5ad6c2ee
Steven Cheng <stevencheng03@hotmail.com>
2020-05-17 22:43:41 -0500

    maybe done with stress testing?

--- genData.java
+++ genData.java
@@ -3,19 +3,13 @@ import java.util.*;
 public class genData {
 	public static void main(String[] args) throws Exception {
 		Random r = new Random();
-		String[] arr = "abcdefghijklmnopqrstuvwxyz".split("");
-		for(int asdf = 1; asdf <= 10; asdf++) {
-			PrintWriter out = new PrintWriter(new FileWriter("stress/prob19-"+asdf+"-in.txt"));
-			int n = r.nextInt(20)*4+r.nextInt(1)*3;
-			int m = r.nextInt(20)+1;
-			out.print(arr[r.nextInt(26)]);
-			for(int i = 1; i < n; i++)
-				out.print(arr[r.nextInt(26)]);
-			out.println();
-			out.print(arr[r.nextInt(26)]);
-			for(int i = 1; i < m; i++)
-				out.print(arr[r.nextInt(26)]);
-			out.println();
+		for(int asdf = 1; asdf <= 1; asdf++) {
+			PrintWriter out = new PrintWriter(new FileWriter("stress/prob22-"+asdf+"-in.txt"));
+			for(int i = 0; i <= 12; i++)
+				for(char c : "AE".toCharArray())
+					out.println(i + " " + c);
+			for(char c : "ABCDEFG".toCharArray())
+				out.println(c);
 			out.close();
 		}
 	}
