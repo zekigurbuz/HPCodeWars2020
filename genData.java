bf0bca07f5bce443ec0f1be614778168050a15f2
Steven Cheng <stevencheng03@hotmail.com>
2020-05-17 22:53:05 -0500

    prob25

--- genData.java
+++ genData.java
@@ -3,13 +3,15 @@ import java.util.*;
 public class genData {
 	public static void main(String[] args) throws Exception {
 		Random r = new Random();
-		for(int asdf = 1; asdf <= 1; asdf++) {
-			PrintWriter out = new PrintWriter(new FileWriter("stress/prob22-"+asdf+"-in.txt"));
-			for(int i = 0; i <= 12; i++)
-				for(char c : "AE".toCharArray())
-					out.println(i + " " + c);
-			for(char c : "ABCDEFG".toCharArray())
-				out.println(c);
+		String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		for(int asdf = 1; asdf <= 5; asdf++) {
+			PrintWriter out = new PrintWriter(new FileWriter("stress/prob25-"+asdf+"-in.txt"));
+			out.println(26 + " " + 26*26);
+			for(char c : alpha.toCharArray())
+				out.printf("%02d%02d %s%n", r.nextInt(99)+1, r.nextInt(99)+1, ""+c);
+			for(char c : alpha.toCharArray())
+				for(char d : alpha.toCharArray())
+					out.println(c + " " + d);
 			out.close();
 		}
 	}
